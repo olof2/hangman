c74aad3efb6e28d95e3ce1f8dea281c4d3d0c0bd
olof2 <o.jonsson95@gmail.com>
2025-09-04 09:53:35 +0200

    starting

--- Program.cs
+++ Program.cs
@@ -1,8 +1,29 @@
-﻿internal class Program
+﻿using System.Text;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
 
+        // *** init
+        int lives = 8;
+        string secretWord = "giraffe";
+        StringBuilder guess = new StringBuilder();
+        for(int i = 0; i < secretWord.Length; i++)
+        {
+            guess.Append("_");
+        }
+
+        // *** gameloop
+        while (true)
+        {
+            Console.WriteLine(guess);
+        }
+
+        // *** end or exit
+
+
+        // *** shutdown and cleanup
 
     }
 }
