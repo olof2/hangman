b8e9f1db8d41b568b3723d865a4ed05ba3ea05fc
olof2 <o.jonsson95@gmail.com>
2025-09-04 11:59:22 +0200

    obsceneties

--- Program.cs
+++ Program.cs
@@ -9,13 +9,15 @@ internal class Program
         int lives = 8;
         string secretWord = "giraffe";
         StringBuilder guess = new StringBuilder();
+        StringBuilder guessedLetters = new StringBuilder();
+
         for(int i = 0; i < secretWord.Length; i++)
         {
             guess.Append("_");
         }
 
         // *** gameloop
-        Console.WriteLine("game starts!");
+        Console.WriteLine("game starts! type '1' to exit");
         while (true)
         {
             Console.WriteLine(guess.ToString() + ". Lives: " + lives.ToString());
@@ -30,12 +32,17 @@ internal class Program
                 continue;
             }
 
+            //creating exit-code
+            if (input == "1") { break; };
+
+
             char letter = input[0];
             Console.WriteLine("you guessed " + letter);
 
 
             // check if guessed letter is correct
             bool correct = false;
+            //bool repeatLetter = false;
             for(int i = 0; i<secretWord.Length; i++)
             {
                 if (letter == secretWord[i])
@@ -44,7 +51,22 @@ internal class Program
                     guess[i] = letter;
                 }
             }
+
+            // check if letter is already guessed
+            for(int i = 0; i < guessedLetters.Length; i++)
+            {
+                if (letter == guessedLetters[i])
+                {
+                    //Console.Clear();
+                    correct = true;
+                    Console.WriteLine("you have already guessed " + letter);
+                }
+            }
+
+            guessedLetters.Append(letter);
+
             if (!correct) { lives--; };
+
 
             if (lives == 0) { Console.WriteLine("out of lives! you lost!"); break; }
             if (secretWord == guess.ToString()) { Console.WriteLine("you won! you have " + lives + " lives left!"); break; }
@@ -52,8 +74,6 @@ internal class Program
 
         // *** end or exit
 
-
-        // *** shutdown and cleanup
 
     }
 }
