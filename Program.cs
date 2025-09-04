f48501268e00993ba4e2f9f7f62651ab02d52838
olof2 <o.jonsson95@gmail.com>
2025-09-04 09:22:52 +0200

    cleanup

--- Program.cs
+++ Program.cs
@@ -2,15 +2,7 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("hallå värld!");
-        int age = 12;
-        Console.WriteLine(age + " är ålder");
-        Thread.Sleep(1000);
-        Console.WriteLine(age+1);
 
-// adding first comment
-// adding second comment abc123 123
-//adding third comment från skolan
 
     }
 }
